21804c817197ac9b0bbecb50319666528b7eb082
tantarusauce <104958869+tantarusauce@users.noreply.github.com>
2024-09-21 22:39:59 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "DxLib.h"
 volatile int EndFlag;
 
-void DrawScreen(int floor, int Lwall, int Rwall, int Box1, int Box2, int Box3, int map[10][10]) {
+void DrawScreen(int floor, int Lwall, int Rwall, int Box1, int Box2, int Box3, int LBox1, int LBox2, int LBox3, int LBox4, int map[10][10]) {
 	int i, j;
 	DrawRotaGraph(440, -62, 1, 0, Rwall, TRUE);
 	DrawRotaGraph(200, -62, 1, 0, Lwall, TRUE);
@@ -22,6 +22,18 @@ void DrawScreen(int floor, int Lwall, int Rwall, int Box1, int Box2, int Box3, i
 			case 4://左奥を向いたボックス
 				DrawRotaGraph(24 * j - i * 24 + 320, 12 * i + 12 * j + 117, 1, 0, Box3, TRUE);
 				break;
+			case 5://左下を向いたラージボックス
+				DrawRotaGraph(24 * j - i * 24 + 309, 12 * i + 12 * j + 121, 1, 0, LBox1, TRUE);
+				break;
+			case 6://右下を向いたラージボックス
+				DrawRotaGraph(24 * j - i * 24 + 332, 12 * i + 12 * j + 122, 1, 0, LBox2, TRUE);
+				break;
+			case 7://左奥を向いたラージボックス
+				DrawRotaGraph(24 * j - i * 24 + 343, 12 * i + 12 * j + 117, 1, 0, LBox3, TRUE);
+				break;
+			case 8://右奥を向いたラージボックス
+				DrawRotaGraph(24 * j - i * 24 + 309, 12 * i + 12 * j + 117, 1, 0, LBox4, TRUE);
+				break;
 			}
 		}
 	}
@@ -31,17 +43,23 @@ void DrawScreen(int floor, int Lwall, int Rwall, int Box1, int Box2, int Box3, i
 DWORD WINAPI MainThread(LPVOID)
 {
 	//初期化
-	int floor, Lwall, Rwall, Box1, Box2, Box3;
+	int floor, Lwall, Rwall, Box1, Box2, Box3, LBox1, LBox2, LBox3, LBox4;
 	int map[10][10] = {0};
+	map[6][7] = 8;
 
 	floor = LoadGraph("./images/floor.png", TRUE);
 	Lwall = LoadGraph("./images/wallleft.png", TRUE);
 	Rwall = LoadGraph("./images/wallright.png", TRUE);
-	Box1 = LoadGraph("./images/shelfLeft.png", TRUE);
-	Box2 = LoadGraph("./images/shelfRight.png", TRUE);
-	Box3 = LoadGraph("./images/shelfBack.png", TRUE);
+	Box1 = LoadGraph("./images/miniShelfLeft.png", TRUE);
+	Box2 = LoadGraph("./images/miniShelfRight.png", TRUE);
+	Box3 = LoadGraph("./images/miniShelfBack.png", TRUE);
+	LBox1 = LoadGraph("./images/miniLargeShelfLeft.png", TRUE);
+	LBox2 = LoadGraph("./images/miniLargeShelfRight.png", TRUE);
+	LBox3 = LoadGraph("./images/miniLargeShelfBackLeft.png", TRUE);
+	LBox4 = LoadGraph("./images/miniLargeShelfBackRight.png", TRUE);
+
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
-		DrawScreen(floor, Lwall, Rwall, Box1, Box2, Box3, map);
+		DrawScreen(floor, Lwall, Rwall, Box1, Box2, Box3, LBox1, LBox2, LBox3, LBox4, map);
 
 	}
 	
